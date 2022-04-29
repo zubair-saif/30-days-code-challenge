044ae26403ce22274def1eee4d5f0649dfe5d257
zubair-saif <zubairsaif700@gmail.com>
2022-04-29 06:18:28 +0500

    better comment

--- plusOne.js
+++ plusOne.js
@@ -19,8 +19,11 @@ console.log(plusOne([9]));
 
 // Example 1:
 
-// Input: digits = [1,2,3]
-// Output: [1,2,4]
+/**
+ * !Input: digits = [1,2,3]
+ * ? Output: [1,2,4]
+ * Output: [1,2,0]
+ * */
 // Explanation: The array represents the integer 123.
 // Incrementing by one gives 123 + 1 = 124.
 // Thus, the result should be [1,2,4].
