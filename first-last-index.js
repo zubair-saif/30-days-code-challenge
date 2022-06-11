3f65619d8fed6fd3563c9fe9edf0a3891ed69563
zubair-saif <zubairsaif700@gmail.com>
2022-06-11 20:03:58 +0500

    ..

--- first-last-index.js
+++ first-last-index.js
@@ -5,16 +5,15 @@ let fun = function (str) {
     let last = '';
     let len = str.length;
     for (let i = 0; i < len; i++) {
-
         if (str.indexOf(str.charAt(i)) === str.lastIndexOf(str.charAt(i))) {
             first += str.charAt(i);
-            last += str[len - 2];
+            last += str.charAt(len - i - 1);
             break;
         }
     }
     console.log('first=' + ' ' + first, 'last=' + ' ' + last);
 
 }
-fun('kamrank');
+fun('zubairz');
 
 // write a function that finds the first and last non repeating alphabets from a string
